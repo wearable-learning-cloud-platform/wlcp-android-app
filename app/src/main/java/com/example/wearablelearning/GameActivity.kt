e4a1425cc3931d2325722e2825f2968641d65459
apoh3 <pohallison@gmail.com>
2022-02-01 08:40:13 -0500

    Text entry transition fragment added with example in game 11

--- app/src/main/java/com/example/wearablelearning/GameActivity.kt
+++ app/src/main/java/com/example/wearablelearning/GameActivity.kt
@@ -141,6 +141,12 @@ class GameActivity : AppCompatActivity() {
             ft.replace(R.id.frameLayout2, fragInfo)
             ft.commit()
         }
+        else if(type.contains("text_entry")) {
+            val fragInfo = TransitionTextEntryFragment()
+            fragInfo.arguments = bundle
+            ft.replace(R.id.frameLayout2, fragInfo)
+            ft.commit()
+        }
         else {
             val fragInfo = TransitionEndGameFragment()
             ft.replace(R.id.frameLayout2, fragInfo)
