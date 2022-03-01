85d51da96ea9d5c2595245766eda8f48f8ec1961
apoh3 <pohallison@gmail.com>
2022-03-01 07:46:12 -0500

    Play and rewind buttons for sound state (issue #213 and #192)

--- app/src/main/java/com/example/wearablelearning/StateSoundFragment.kt
+++ app/src/main/java/com/example/wearablelearning/StateSoundFragment.kt
@@ -7,13 +7,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import com.google.android.material.button.MaterialButton
 
 class StateSoundFragment : Fragment() {
     companion object {
         var mediaPlayer = MediaPlayer()
+        var isPaused = true
     }
 
     override fun onCreateView(
@@ -32,16 +35,54 @@ class StateSoundFragment : Fragment() {
         val resID = resources.getIdentifier(soundName, "raw", context?.packageName)
 
         mediaPlayer = MediaPlayer.create(context, resID)
-        mediaPlayer.start()
+
+        val rewindButton = view.findViewById<MaterialButton>(R.id.rewind_btn)
+
+        rewindButton.setOnClickListener { v ->
+            var currPos = mediaPlayer.currentPosition
+
+            if(currPos < 0) {
+                currPos = 0
+            }
+
+            mediaPlayer.seekTo(currPos - 10000)
+        }
+
+        val playButton = view.findViewById<MaterialButton>(R.id.play_btn)
+
+        playButton.setOnClickListener { v ->
+            if(isPaused) {
+                isPaused = false
+                mediaPlayer.start()
+
+                playButton.icon = context?.let { ContextCompat.getDrawable(it, R.drawable.ic_pause_foreground)}
+            }
+            else {
+                isPaused = true
+
+                if(mediaPlayer.isPlaying) {
+                    mediaPlayer.pause()
+                }
+
+                playButton.icon = context?.let { ContextCompat.getDrawable(it, R.drawable.ic_play_foreground)}
+            }
+        }
+
+        mediaPlayer?.setOnCompletionListener {
+            mediaPlayer.seekTo(0)
+            isPaused = true
+            playButton.icon = context?.let { ContextCompat.getDrawable(it, R.drawable.ic_play_foreground)}
+        }
     }
 
     override fun onPause() {
         super.onPause()
 
-        if(mediaPlayer.isPlaying()) {
+        if(mediaPlayer.isPlaying) {
             mediaPlayer.stop()
-            mediaPlayer.release()
-            mediaPlayer.release()
         }
+
+        mediaPlayer.reset()
+        mediaPlayer.release()
     }
 }
