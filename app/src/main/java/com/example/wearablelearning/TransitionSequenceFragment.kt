31b5ddcc3d3c967ea57f221c25d3517cfd3e6e36
Vivienne Tam <vivienne.tam2020@gmail.com>
2022-06-02 17:53:19 -0400

    Adding back in missing imports from TransitionSequenceFragment.kt

--- app/src/main/java/com/example/wearablelearning/TransitionSequenceFragment.kt
+++ app/src/main/java/com/example/wearablelearning/TransitionSequenceFragment.kt
@@ -1,5 +1,8 @@
 package com.example.wearablelearning
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
