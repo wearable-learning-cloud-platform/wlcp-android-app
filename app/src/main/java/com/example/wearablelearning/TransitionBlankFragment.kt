66d283e6800625b6e66ee565381b600ed44c855d
apoh3 <pohallison@gmail.com>
2022-06-28 14:45:00 -0400

    Updated documentation for TransitionBlankFragment.

--- app/src/main/java/com/example/wearablelearning/TransitionBlankFragment.kt
+++ app/src/main/java/com/example/wearablelearning/TransitionBlankFragment.kt
@@ -1,14 +1,17 @@
 package com.example.wearablelearning
 
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.material.textview.MaterialTextView
 
+/**
+ * The [TransitionBlankFragment] class is called by [GameActivity]. This fragment is blank and is
+ * used when there is only one transition fragment. The only time this fragment is not used is if
+ * there are more than one transition fragments (e.g. timer and button press). This fragment allows
+ * for no user actions and is simple a place holder.
+ */
 class TransitionBlankFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
