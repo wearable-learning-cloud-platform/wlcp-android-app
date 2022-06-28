aff654dac8b43aa3cd0423cb75a5a3dd52dc4eae
apoh3 <pohallison@gmail.com>
2022-06-28 14:33:33 -0400

    Updated documentation for StateVideoFragment.

--- app/src/main/java/com/example/wearablelearning/StateVideoFragment.kt
+++ app/src/main/java/com/example/wearablelearning/StateVideoFragment.kt
@@ -2,15 +2,12 @@ package com.example.wearablelearning
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.media.MediaPlayer.OnPreparedListener
 import android.net.Uri
 import android.os.Bundle
 import android.os.SystemClock
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
-import android.view.View.OnTouchListener
 import android.view.ViewGroup
 import android.widget.MediaController
 import android.widget.TextView
@@ -18,9 +15,11 @@ import android.widget.VideoView
 import androidx.fragment.app.Fragment
 import com.google.android.material.button.MaterialButton
 
-
+/**
+ * The [StateVideoFragment] class is called by [GameActivity]. This fragment holds a video.
+ */
 class StateVideoFragment : Fragment() {
-    lateinit var videoView: VideoView
+    private lateinit var videoView: VideoView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,9 +30,11 @@ class StateVideoFragment : Fragment() {
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        /** Retrieve the text from the bundle. This may be null/empty in which case no text is displayed. */
         val stateContent = this.requireArguments().getString("text")
         getView()?.findViewById<TextView>(R.id.state_tv)?.text = stateContent
 
+        /** Retrieve the video name from the bundle. */
         val stateVideo = this.requireArguments().getString("video")
         val videoName = stateVideo.toString().split(".")[0]
         val resID = resources.getIdentifier(videoName, "raw", context?.packageName)
@@ -47,28 +48,30 @@ class StateVideoFragment : Fragment() {
         var videoPath = ""
         var videoOrientation = ""
 
+        /** Get the path to the video resource. */
         if (videoView != null) {
             videoPath = "android.resource://" + context?.packageName + "/" + resID
             videoView.setMediaController(mediaController)
             videoView.setVideoURI(Uri.parse(videoPath))
-
-
         }
 
-        videoView.setOnPreparedListener(OnPreparedListener { mp ->
-            mp.setOnVideoSizeChangedListener { mp, width, height ->
+        /** Determine the orientation of the video. */
+        videoView.setOnPreparedListener { mp ->
+            mp.setOnVideoSizeChangedListener { _, _, _ ->
                 val width: Int = videoView.width
                 val height: Int = videoView.height
 
-                videoOrientation = if(width >= height) {
+                videoOrientation = if (width >= height) {
                     "landscape"
                 } else {
                     "portrait"
                 }
             }
-        })
+        }
 
-        videoView?.setOnTouchListener(OnTouchListener { v, event ->
+        /** When video is touched, the video will "expand" to fullscreen by calling
+         * [VideoFullscreenActivity]. */
+        videoView.setOnTouchListener { v, event ->
             when (event?.action) {
                 MotionEvent.ACTION_DOWN -> {
                     val intent = Intent(activity, VideoFullscreenActivity::class.java)
@@ -79,8 +82,10 @@ class StateVideoFragment : Fragment() {
             }
 
             v?.onTouchEvent(event) ?: true
-        })
+        }
 
+        /** The play button overlays the video so that when the video is touched it acts like
+         * a button an triggers the expansion of the video to fullscreen.. */
         getView()?.findViewById<MaterialButton>(R.id.play_btn)?.setOnClickListener {
             val motionEvent = MotionEvent.obtain(
                 SystemClock.uptimeMillis(),
@@ -91,10 +96,13 @@ class StateVideoFragment : Fragment() {
                 0
             )
 
-            videoView?.dispatchTouchEvent(motionEvent)
+            videoView.dispatchTouchEvent(motionEvent)
         }
     }
 
+    /**
+     * The [onResume] function resumes the video by starting at the beginning.
+     */
     override fun onResume() {
         super.onResume()
 
