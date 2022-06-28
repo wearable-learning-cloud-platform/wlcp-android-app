b520e1bb9705a9595a66abbdbe98574730b34fa2
apoh3 <pohallison@gmail.com>
2022-06-28 14:09:43 -0400

    Updated documentation for LoginWithNameFragment.

--- app/src/main/java/com/example/wearablelearning/LoginWithNameFragment.kt
+++ app/src/main/java/com/example/wearablelearning/LoginWithNameFragment.kt
@@ -1,14 +1,11 @@
 package com.example.wearablelearning
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import androidx.fragment.app.Fragment
-import java.util.*
-
 
 /**
  * The [LoginWithNameFragment] class is called by [LoginActivity].
@@ -17,7 +14,6 @@ import java.util.*
  * WearableLearning app with a name.
  */
 class LoginWithNameFragment : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -28,10 +24,18 @@ class LoginWithNameFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        /**
+         * The _gameInfo_ is a [GameInfo] object and is used to track user input about the game
+         * (e.g., gamePin, name, etc. - See the [GameInfo] class for all relevant fields).
+         * All [GameInfo] variables are null at startup and get populated as the user moves
+         * through the different app Activities; [GameInfo] objects are passed from one Activity
+         * to the next.
+         */
         val intent = requireActivity().intent
-        var gameInfo = intent.getSerializableExtra("gameInfo") as? GameInfo
+        val gameInfo = intent.getSerializableExtra("gameInfo") as? GameInfo
 
-        val editTextName = view?.findViewById<EditText>(R.id.loginNameEditText)
+        /** The _editTextName_ is used to allow the user to enter a name. */
+        val editTextName = view.findViewById<EditText>(R.id.loginNameEditText)
 
         if (gameInfo?.name != null && editTextName != null) {
             editTextName.setText(gameInfo.name)
