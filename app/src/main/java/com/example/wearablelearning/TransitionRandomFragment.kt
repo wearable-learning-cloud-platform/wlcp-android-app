c53a13c69b591341f73fe3cdafddb0be73c81863
apoh3 <pohallison@gmail.com>
2022-07-01 14:01:32 -0400

    Updated documentation for TransitionRandomFragment.

--- app/src/main/java/com/example/wearablelearning/TransitionRandomFragment.kt
+++ app/src/main/java/com/example/wearablelearning/TransitionRandomFragment.kt
@@ -1,12 +1,13 @@
 package com.example.wearablelearning
 
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.annotation.RequiresApi
 
 class TransitionRandomFragment : Fragment() {
     override fun onCreateView(
@@ -16,23 +17,36 @@ class TransitionRandomFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_transition_random, container, false)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        /** The _id_ is the current transition's id. */
         var id = this.requireArguments().getString("id")
 
         val continueButton = view.findViewById<Button>(R.id.continue_btn)
 
-        var gameInfo = (activity as GameActivity).gameInfo
+        /**
+         * Retrieve the [GameInfo] object from the intent that started [GameActivity].
+         *
+         * The _gameInfo_ is a [GameInfo] object and is used to track user input about the game
+         * (e.g., gamePin, name, etc. - See the [GameInfo] class for all relevant fields).
+         */
+        val gameInfo = (activity as GameActivity).gameInfo
 
+        /**
+         * If only button in fragment is selected, then callTransition in [GameActivity] is called
+         * on a random transition selected from a list of possible transitions listed in the
+         * json.
+         */
         continueButton.setOnClickListener {
             gameInfo.interactionType = "continueBtnPress"
 
-            var transitions = id?.split(";;")
-            var transitionsCnt = transitions?.size
-            var randomIdx = (0 until transitionsCnt!!).random()
+            val transitions = id?.split(";;")
+            val transitionsCnt = transitions?.size
+            val randomIdx = (0 until transitionsCnt!!).random()
 
-            id = transitions?.get(randomIdx)
+            id = transitions[randomIdx]
 
             id?.let { transId -> (activity as GameActivity).callTransition(transId, false, "", "random") }
         }
